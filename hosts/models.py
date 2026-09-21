ee2e8ec7845afff5eae38b1e15ddf2950a80997c
kcisner <kevingael.cisneros047@gmail.com>
2026-09-21 16:03:10 -0600

    Add initial migration and model definition for Host entity

--- hosts/models.py
+++ hosts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Host(models.Model):
+    nombre = models.CharField(max_length=100)
+    hostname = models.CharField(max_length=64)
+    ip_address = models.GenericIPAddressField(protocol='IPv4', unique=True)
+    puerto = models.IntegerField()
+    intervalo_chequeo = models.TimeField()
+    propietario = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='hosts'
+    )
+    activo = models.BooleanField(default=True)
